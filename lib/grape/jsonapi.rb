6ad7ad7777f7e3a5f0b63af1cecefbc2f403646a
Alex Shook <alex.shook@salido.com>
2017-11-09 18:50:24 -0500

    Require the new services directory so we can use services

--- lib/grape/jsonapi.rb
+++ lib/grape/jsonapi.rb
@@ -4,6 +4,7 @@ require 'grape/jsonapi/exposer'
 require 'grape/jsonapi/entity'
 require 'grape/jsonapi/formatter'
 require 'grape/jsonapi/parameters'
+require 'grape/jsonapi/services'
 
 module Grape
   module Jsonapi
